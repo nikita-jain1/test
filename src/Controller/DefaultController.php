d5268432a935c887b8b11ddc7b99f8d149501643
nikita jain <nikita.jain@happiestminds.com>
2023-01-09 20:31:11 +0530

    added img

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends FrontendController
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function defaultAction(Request $request): Response
+    {
+        return $this->render('default/default.html.twig');
+    }
+
+     /**
+     * @param Request $request
+     * @return Response
+     */
+    public function headerAction(Request $request): Response
+    {
+        return $this->render('default/header.html.twig');
+    }
+}
